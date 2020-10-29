abaac89ae6460edf28731dcdb77acd96f41eae27
vijaynan <73652394+vijaynan@users.noreply.github.com>
2020-10-29 21:11:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,61 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
 import pickle
 
+# load the model from disk
+filename = 'nlp_model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+cv=pickle.load(open('tranform.pkl','rb'))
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+	return render_template('home.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+#	df= pd.read_csv("spam.csv", encoding="latin-1")
+#	df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
+#	# Features and Labels
+#	df['label'] = df['class'].map({'ham': 0, 'spam': 1})
+#	X = df['message']
+#	y = df['label']
+#	
+#	# Extract Feature With CountVectorizer
+#	cv = CountVectorizer()
+#	X = cv.fit_transform(X) # Fit the Data
+#    
+#    pickle.dump(cv, open('tranform.pkl', 'wb'))
+#    
+#    
+#	from sklearn.model_selection import train_test_split
+#	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+#	#Naive Bayes Classifier
+#	from sklearn.naive_bayes import MultinomialNB
+#
+#	clf = MultinomialNB()
+#	clf.fit(X_train,y_train)
+#	clf.score(X_test,y_test)
+#    filename = 'nlp_model.pkl'
+#    pickle.dump(clf, open(filename, 'wb'))
+    
+	#Alternative Usage of Saved Model
+	# joblib.dump(clf, 'NB_spam_model.pkl')
+	# NB_spam_model = open('NB_spam_model.pkl','rb')
+	# clf = joblib.load(NB_spam_model)
 
-    output = round(prediction[0], 2)
+	if request.method == 'POST':
+		message = request.form['message']
+		data = [message]
+		vect = cv.transform(data).toarray()
+		my_prediction = clf.predict(vect)
+	return render_template('result.html',prediction = my_prediction)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+	app.run(debug=True)
